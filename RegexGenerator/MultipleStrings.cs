d2cc7ee458d333d31796f1faae944c80078f744c
Lan Lu <lan.lu@laserfiche.com>
2017-11-20 16:37:34 -0800

    changed how sample is genereated if a regex is given ,so if '\d|s' is given \d and s will both have 0.5 possibility to be chosen in regex

--- RegexGenerator/MultipleStrings.cs
+++ RegexGenerator/MultipleStrings.cs
@@ -14,13 +14,13 @@ namespace RegexGenerator
         public static void Test()
         {
             List<string> inputs = new List<string>();
-            inputs.Add("AB23456d-234s");
-            inputs.Add("AC999999-9998");
-            inputs.Add("BB34234c-223");
-            inputs.Add("CD34234h-1000");
-            //inputs.Add("(213)304-1072");
-            //inputs.Add("234-345-3432");
-            //inputs.Add("2153456789");
+            //inputs.Add("AB23456d-234s");
+            //inputs.Add("AC999999-9998");
+            //inputs.Add("BB34234c-223");
+            //inputs.Add("CD34234h-1000");
+            inputs.Add("(213)304-1072");
+            inputs.Add("234-345-3432");
+            inputs.Add("2153456789");
             List<List<string>> suggestions;
             GiveSuggestions(inputs, out suggestions);
 
@@ -33,44 +33,96 @@ namespace RegexGenerator
                 }
                 Console.Write("\n\t");
 
-                for (int i = 0; i < 10; ++i)
+                List<string> output = new List<string>();
+                PrintHelper(suggest, 0, string.Empty, output);
+                foreach (var str in output)
                 {
-
-                    List<string> output = new List<string>();
-                    PrintHelper(suggest, out output);
-                    foreach (var ch in output)
-                    {
-                        Console.Write("{0}", ch);
-                    }
-                    Console.Write("\n\t");
+                    Console.Write("{0}\n\t", str);
                 }
                 Console.Write("\n");
+
             }
             return;
 
         }
 
-        public static void PrintHelper(List<string> input, out List<string> output)
+        public static void PrintHelper(List<string> input, int index, string curResult, List<string> results)
         {
-            output = new List<string>();
-
-            Random ran = new Random(Guid.NewGuid().GetHashCode());
-
-            foreach (string symbol in input)
+            // use List of candidates to get all the available combinations
+            if (input.Count <= index)
             {
-                while (true)
+                results.Add(curResult);
+                return;
+            }
+            var candidates = GetCandidates(input[index]);
+
+            foreach (string candidate in candidates)
+            {
+                PrintHelper(input, index + 1, curResult + candidate, results);
+            }
+            return; 
+        }
+
+        private static List<string> GetCandidates(string input)
+        {
+            // cut input based on regex
+            // read until a | or ) is met
+            // randomly pick one ascii character and match the part-of-regex
+            // if matches, put it in candidate list
+            // add empty string to the list if it is also a match
+
+            int index = 0;
+            List<string> candidates = new List<string>();
+
+            // corner case string.Empty
+            Regex regex = new Regex(input);
+            MatchCollection matches = regex.Matches(string.Empty);
+            if (hasMatch(matches, string.Empty))
+            {
+                candidates.Add(string.Empty);
+            }
+            // add candidates
+            while (index < input.Length)
+            {
+                while (index < input.Length && (input[index] == '(' || input[index] == ')' || input[index] == '|' || input[index] == '?'))
                 {
-                    int value = ran.Next() % 250 + 33;
-                    string ascii = value < 255 ? ((char)value).ToString() : string.Empty;
-                    Regex regex = new Regex(symbol);
-                    MatchCollection matches = regex.Matches(ascii);
-                    if (hasMatch(matches, ascii))
+                    ++index;
+                }
+
+                string part = string.Empty;
+                if (index < input.Length && (input[index] != '(' && input[index] != ')' && input[index] != '|' && input[index] != '?'))
+                {
+                    while (index < input.Length && (input[index] != '(' && input[index] != ')' && input[index] != '|' && input[index] != '?'))
                     {
-                        output.Add(ascii);
-                        break;
+                        part += input[index];
+                        if (input[index] == '\\')
+                        {
+                            ++index;
+                            if (index < input.Length)
+                            {
+                                part += input[index];
+                            }
+                        }
+                        ++index;
+                    }
+
+                    Random ran = new Random(Guid.NewGuid().GetHashCode());
+                    while (true)
+                    {
+                        int value = ran.Next() % 223 + 32;
+                        string ascii = ((char)value).ToString();
+                        regex = new Regex(part);
+                        matches = regex.Matches(ascii);
+                        if (hasMatch(matches, ascii))
+                        {
+                            candidates.Add(ascii);
+                            break;
+                        }
                     }
                 }
             }
+
+            return candidates;
         }
 
         private static bool hasMatch(MatchCollection collection, string value)
